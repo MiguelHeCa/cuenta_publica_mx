e6f57e72d70832aa9a158b5f9fde6ad4855cd14b
Miguel Hernandez <jhernandcab@gmail.com>
2019-04-14 20:43:43 -0500

    Ammending variable 'EJERCIDO_MDP'

--- cuenta_publica_ramo_mod_pp.R
+++ cuenta_publica_ramo_mod_pp.R
@@ -1,19 +1,16 @@
 library(tidyverse)
 
-
-cp <- readRDS("data/cuenta_publica.rds")
-
-saveRDS(cp, "data/cuenta_publica_0817.rds")
+cp <- readRDS("data/cuenta_publica_0817.rds")
 
 # Cuenta pública por ramo
 ramos <- cp %>% 
   group_by(CICLO, ID_RAMO, DESC_RAMO) %>% 
-  summarise(EJERCIDO_MPD = sum(MONTO_EJERCIDO) / 1000000)
+  summarise(EJERCIDO_MDP = sum(MONTO_EJERCIDO) / 1000000)
 
 # Cuenta pública por modalidad
 modalidad <- cp %>% 
   group_by(CICLO, ID_RAMO, DESC_RAMO, ID_MODALIDAD, DESC_MODALIDAD) %>% 
-  summarise(EJERCIDO_MPD = sum(MONTO_EJERCIDO) / 1000000)
+  summarise(EJERCIDO_MDP = sum(MONTO_EJERCIDO) / 1000000)
 
 # Cuenta pública por programa presupuestario
 prog_pres <- cp %>% 
@@ -25,9 +22,10 @@ prog_pres <- cp %>%
            DESC_MODALIDAD,
            ID_PP,
            DESC_PP) %>% 
-  summarise(EJERCIDO_MPD = sum(MONTO_EJERCIDO) / 1000000)
+  summarise(EJERCIDO_MDP = sum(MONTO_EJERCIDO) / 1000000)
 
 # Escribirlo a csv
 write_csv(ramos, "data/ramos.csv")
 write_csv(modalidad, "data/modalidad.csv")
 write_csv(prog_pres, "data/prog_pres.csv")
+
